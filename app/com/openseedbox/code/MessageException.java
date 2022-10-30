f84c92d2b87161828f84a670011a062adcabfdf1
Szabolcs Hubai <szab.hu@gmail.com>
2022-10-30 13:53:20 +0100

    code: MessageException extends PlayExceptionWithJavaSource
    instead just RuntimeException. PlayExceptionWithJavaSource
    was introduced in Play! Framework 1.3 by commit playframework/play1@c38c7934ae15895fafe646fcdbf35f0c3a8e7927
    
    This helps development as in DEV mode it shows the Framework's
    Exception UI with Java source, and keeps it's mouth in PROD mode.
    
    See openseedbox/openseedbox#75

--- app/com/openseedbox/code/MessageException.java
+++ app/com/openseedbox/code/MessageException.java
@@ -1,15 +1,23 @@
 package com.openseedbox.code;
 
-public class MessageException extends RuntimeException {
+import play.exceptions.PlayExceptionWithJavaSource;
+
+public class MessageException extends PlayExceptionWithJavaSource {
+	public MessageException(String message, Throwable t) {
+		super(message,t);
+	}
+
+	@Override
+	public String getErrorTitle() {
+		return getMessage();
+	}
+
+	@Override
+	public String getErrorDescription() {
+		return getCause().getMessage();
+	}
+
 	public MessageException(String message) {
 		super(message);
-	}
-	
-	public MessageException(String message, Object... args) {
-		super(String.format(message, args));
-	}
-	
-	public MessageException(Throwable t, String message, Object... args) {
-		super(String.format(message, args), t);
 	}
 }
